781535551f90012f83ca13d7249ed5bdc7979ce5
Pavan Jain <013pavanjain@gmail.com>
2024-07-23 23:32:27 +0530

    Adds goodbye mail for a leaving user

--- notehub/main/signals.py
+++ notehub/main/signals.py
@@ -1,11 +1,11 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django.core.cache import cache
 from django.conf import settings
 from .models import SocialUsersProfile, UserProfile
 from datetime import datetime
-from .utils import send_verification_email
+from .utils import send_verification_email, send_email
 
 @receiver(post_save, sender=User)
 def user_created_handler(sender, instance, created, **kwargs):
@@ -22,3 +22,11 @@ def user_created_handler(sender, instance, created, **kwargs):
             )
             user_profile_doc.save()
             social_user_profile_doc.delete()
+
+@receiver(post_delete, sender=User)
+def user_deleted_handler(sender, instance, **kwargs):
+    recipients = [instance.username]
+    subject = 'Sorry to see you go!'
+    template = 'goodbye_user.html'
+    metadata = {'user': instance}
+    send_email(recipients, subject, template, metadata)
