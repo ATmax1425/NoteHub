6d60faa9bd0333f9c069215fb1a931f87148de95
Pavan Jain <013pavanjain@gmail.com>
2024-07-24 21:55:34 +0530

    Fixes urls and adapters to connect google account with normal account

--- notehub/main/adapters.py
+++ notehub/main/adapters.py
@@ -1,16 +1,35 @@
 from datetime import datetime
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter 
 from .models import SocialUsersProfile
+from allauth.core.exceptions import ImmediateHttpResponse
 from django.contrib.auth.models import User
+from allauth.socialaccount.models import SocialAccount
+from django.shortcuts import redirect
+from django.contrib.auth import login as login_user
 
-class CustomAccountAdapter(DefaultSocialAccountAdapter ):
+AUTH_BACKEND = 'django.contrib.auth.backends.ModelBackend'
+
+class CustomAccountAdapter(DefaultSocialAccountAdapter):
+
+    def pre_social_login(self, request, sociallogin):
+        email = sociallogin.account.extra_data.get('email')
+        if email:
+            user = None
+            try:
+                user = User.objects.get(email=email)
+            except Exception as exc:
+                print('New user signup')
+            if user and not SocialAccount.objects.filter(user=user, provider=sociallogin.account.provider).exists():
+                sociallogin.connect(request, user)
+                login_user(request, user, AUTH_BACKEND)
+                raise ImmediateHttpResponse(redirect('index'))
+
     def populate_user(self, request, sociallogin, data):
         user = super().populate_user(request, sociallogin, data)
         user.username = user.email
 
         try:
             existing_user = User.objects.get(email=user.email)
-            print("user already exists")
         except:
             extra_data = sociallogin.account.extra_data
             picture_url = extra_data.get('picture')
