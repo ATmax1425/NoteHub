ea7c37a875ade8d5e6b995f69750c4fafb594351
Pavan Jain <013pavanjain@gmail.com>
2024-07-26 21:32:49 +0530

    user profile image in header

--- notehub/main/models.py
+++ notehub/main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     profile_url = models.URLField(default="https://cdn.pixabay.com/photo/2012/04/26/19/43/profile-42914_640.png")
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
--- notehub/main/templatetags/main_filters.py
+++ notehub/main/templatetags/main_filters.py
@@ -14,3 +14,16 @@ def is_password_set_message(message):
     if re.search(PASSWORD_SET_REGEX, str(message)):
         return True
     return False
+
+@register.filter
+def customise_url(image_url, size):
+    google_drive_domain = 'drive.google.com'
+    google_user_content = "lh3.googleusercontent.com"
+    if google_drive_domain in image_url:
+        image_url = image_url.replace('file/d/', 'thumbnail?id=')
+        image_url = image_url.replace('/view', f'&sz=s{size}')
+    elif google_user_content:
+        pattern = r'(?<=s)\d+(?=-c)'
+        image_url = re.sub(pattern, str(size), image_url)
+
+    return image_url
