0854f8cc05a356e5f6f3c0a7bb9222f5fe7ee6d6
Pavan Jain <013pavanjain@gmail.com>
2024-08-11 16:06:03 +0530

    Static files collected

--- notehub/staticfiles/js/scripts.js
+++ notehub/staticfiles/js/scripts.js
@@ -40,3 +40,25 @@ function timeAgo(timestamp) {
         return seconds === 1 ? "1 second ago" : `${seconds} seconds ago`;
     }
 }
+
+function convertSize(sizeBytes) {
+    if (sizeBytes === 0) {
+        return "0 B";
+    }
+
+    const sizeNames = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    const i = Math.floor(Math.log(sizeBytes) / Math.log(1024));
+    const size = (sizeBytes / Math.pow(1024, i)).toFixed(2);
+
+    return `${size} ${sizeNames[i]}`;
+}
+
+function convertDriveFileUrl(sourceUrl) {
+    const fileIdMatch = sourceUrl.match(/\/d\/(.+?)\//);
+    if (fileIdMatch && fileIdMatch[1]) {
+        const fileId = fileIdMatch[1];
+        const targetUrl = `https://drive.usercontent.google.com/u/0/uc?id=${fileId}&export=download`;
+        return targetUrl;
+    }
+    return null;
+}
